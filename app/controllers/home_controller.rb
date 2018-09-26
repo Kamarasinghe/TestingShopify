ba61bd9cbfbdbfed4802507fc606b7c96d36156f
Kav <kamarasinghe@nevada.unr.edu>
2018-09-26 14:10:26 -0700

    Messing with Shopify

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,14 @@ class HomeController < ShopifyApp::AuthenticatedController
     @products = ShopifyAPI::Product.find(:all, params: { limit: 2 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
 
-    product_count = ShopifyAPI::Product.count()
-    products_json = @products.to_json
-    SaveToDbJob.perform_later(products_json, product_count)
+    products_1 = ShopifyAPI::Product.find(:all, params: { limit: 2, page: 1 })
+    products_2 = ShopifyAPI::Product.find(:all, params: { limit: 2, page: 2 })
+
+    print products_1
+    puts "THIS ARE PRODUCTS"
+    print products_2
+    # product_count = ShopifyAPI::Product.count()
+    # products_json = @products.to_json
+    # SaveToDbJob.perform_later(products_json, product_count)
   end
 end
