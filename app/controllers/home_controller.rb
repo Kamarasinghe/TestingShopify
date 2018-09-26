266f7198fd7b423f40a91a4117da8a95c7b9781f
Kav <kamarasinghe@nevada.unr.edu>
2018-09-26 16:07:25 -0700

    Messing with Shopify

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,12 +8,16 @@ class HomeController < ShopifyApp::AuthenticatedController
 
     product_count = ShopifyAPI::Product.count()
     i = 1
-    while (i <= product_count) do
-      products = ShopifyAPI::Product.find(:all, :params => { :limit => 2, :page => i })
-      products_json = products.to_json
-      SaveToDbJob.perform_later(products_json)
-      i += 1
-    end
+    # while (i <= product_count) do
+    #   products = ShopifyAPI::Product.find(:all, :params => { :limit => 2, :page => i })
+    #   products_json = products.to_json
+    #   SaveToDbJob.perform_later(products_json)
+    #   i += 1
+    # end
+
+    save_product = { title: 'TEST FROM CONTROLLER', description: 'This is a description', vendor: 'Test Vendor' }
+    product_to_save = Product.new(save_product)
+    product_to_save.save
     
     # products_json = @products.to_json
     # SaveToDbJob.perform_later(products_json, product_count)
--- app/jobs/save_to_db_job.rb
+++ app/jobs/save_to_db_job.rb
@@ -4,11 +4,11 @@ class SaveToDbJob < ApplicationJob
   def perform(products)
     products_parsed = JSON.parse(products)
     
-    products_parsed.each_with_index do |product, idx| 
-      save_product = { :title => product.title, :description => product.body_html, :vendor => product.vendor }
-      product_to_save = Product.new(save_product)
-      product_to_save.save
-    end
+    # products_parsed.each_with_index do |product, idx| 
+    #   save_product = { :title => product.title, :description => product.body_html, :vendor => product.vendor }
+    #   product_to_save = Product.new(save_product)
+    #   product_to_save.save
+    # end
 
   end
 end
