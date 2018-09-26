57eb458e39b8de00889cd9bdb5f0f7fabff4860b
Kav <kamarasinghe@nevada.unr.edu>
2018-09-26 15:32:52 -0700

    Messing with Shopify

--- app/jobs/save_to_db_job.rb
+++ app/jobs/save_to_db_job.rb
@@ -5,7 +5,7 @@ class SaveToDbJob < ApplicationJob
     products_parsed = JSON.parse(products)
     
     products_parsed.each_with_index do |product, idx| 
-      save_product = Product.new
+      save_product = Product.new()
       save_product.title = product.title
       save_product.description = product.body_html
       save_product.vendor = product.vendor
