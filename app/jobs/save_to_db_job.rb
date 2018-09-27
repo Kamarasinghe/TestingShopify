0881d6d014e2a4eb9e7ccde27ca94ba931476ba8
Kav <kamarasinghe@nevada.unr.edu>
2018-09-27 11:02:41 -0700

    Messing with Shopify

--- app/jobs/save_to_db_job.rb
+++ app/jobs/save_to_db_job.rb
@@ -15,12 +15,15 @@ class SaveToDbJob < ApplicationJob
       variant_price = variant['price']
       variant_position = variant['position']
 
-      Variant.where(product_id: product_id, title: variant_title).first_or_create({
-        product_id: product_id,
-        title: variant_title,
-        price: variant_price,
-        position: variant_position
-      })
+      Variant.where([
+        'product_title = :product_title and title = :title', 
+        { product_title: product_title, title: variant_title }
+        ]).first_or_create({
+          product_id: product_id,
+          title: variant_title,
+          price: variant_price,
+          position: variant_position
+        })
     end
   end
 
