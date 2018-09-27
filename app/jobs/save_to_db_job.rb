8ec0d9ccd49826da4ebfe551808f3f34830dfa9f
Kav <kamarasinghe@nevada.unr.edu>
2018-09-26 20:43:13 -0700

    Messing with Shopify

--- app/jobs/save_to_db_job.rb
+++ app/jobs/save_to_db_job.rb
@@ -5,18 +5,12 @@ class SaveToDbJob < ApplicationJob
     products_parsed = JSON.parse(products)
     
     products_parsed.each do |product|
-      puts product
       product_title = product['title']
       product_desc = product['body_html']
       product_vendor = product['vendor']
-      
-      print 'PRODUCT TITLE', product_title
-      puts ''
-      print 'PRODUCT DESCRIPTION', product_desc
-      puts ''
-      print 'PRODUCT VENDOR', product_vendor
 
-      # save_product = { :title => product.title, :description => product.body_html, :vendor => product.vendor }
+      save_product = { title: product_title, description: product_desc, vendor: product_vendor }
+      print 'THIS IS SAVE PRODUCT', save_product
       # product_to_save = Product.new(save_product)
       # product_to_save.save
     end
