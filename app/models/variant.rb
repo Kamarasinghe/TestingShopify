93e43832cc9bab1d215ebac1de7a6a7e02052872
Kav <kamarasinghe@nevada.unr.edu>
2018-09-28 10:08:30 -0700

    Messing with Shopify

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -1,5 +1,5 @@
 class Variant < ApplicationRecord
     # has_one :image, dependent: :destroy
-    belongs_to :product, foreign_key: :product_id, primary_key: variant_id
+    belongs_to :product, foreign_key: :product_id
     has_one :image, primary_key: :variant_id
 end
